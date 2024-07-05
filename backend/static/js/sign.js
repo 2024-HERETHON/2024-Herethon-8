dcbf48b8879361d130693c30db42e66330d8813d
SangHee-Hong <hongdabagi@gmail.com>
2024-07-06 04:52:22 +0900

    dkljasdlfkjsad

--- backend/static/js/sign.js
+++ backend/static/js/sign.js
@@ -4,9 +4,9 @@ function togglePassword(passwordId, toggleId) {
 
     if (passwordInput.type === "password") {
         passwordInput.type = "text";
-        togglePassword.src = "img/pwdhidebtn.png"; // 눈 감은 아이콘으로 변경
+        togglePassword.src = "../../static/img/eyeopen.png"; // 눈 감은 아이콘으로 변경
     } else {
         passwordInput.type = "password";
-        togglePassword.src = "img/pwdshowbtn.png"; // 눈 뜬 아이콘으로 변경
+        togglePassword.src = "../../static/img/pwdshowbtn.png"; // 눈 뜬 아이콘으로 변경
     }
 }
